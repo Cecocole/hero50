8599d725ac2343136d0e97f2e05c3fb9c5509be7
gaoqianshan <645992686@qq.com>
2018-09-02 11:18:46 +0800

    配置主页文件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+//基于webpack的开发，是基于模块化的开发
+// node.js的模块是遵守CommonJS规范
+// var Vue = require('vue')
+
+// ES6模块化的语法 
 import Vue from 'vue'
 import App from './App.vue'
-
+//导入全局样式
+// 导入bootstrao样式
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
 new Vue({
   el: '#app',
   render: h => h(App)
